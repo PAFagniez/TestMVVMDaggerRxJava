43f21d214f72b7e5a10879d220e6a206b2a45e8a
PAFagniez <pierreantoine.fagniez@gmail.com>
2019-07-07 19:10:00 +0200

    Add ui test to finish

--- app/src/androidTest/java/com/example/testmvvmdaggerrx/MilkywayImageFragmentTest.kt
+++ app/src/androidTest/java/com/example/testmvvmdaggerrx/MilkywayImageFragmentTest.kt
@@ -1,18 +1,13 @@
 package com.example.testmvvmdaggerrx
 
-import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
 import android.support.test.runner.AndroidJUnit4
 import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.assertion.ViewAssertions.*
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.*
-import com.bumptech.glide.Glide
-import com.bumptech.glide.GlideBuilder
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.example.testmvvmdaggerrx.app.ui.MainActivity
 import com.example.testmvvmdaggerrx.app.ui.milkywayimages.fragment.MilkywayImageFragment
 import com.example.testmvvmdaggerrx.app.utils.DateUtils
